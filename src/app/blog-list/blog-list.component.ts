73a88dc455e4e20ad46f10e87144ca31bb54ceb6
Witold Wojnarowicz <witold.wojnarowicz@gds.ey.com>
2019-07-03 16:31:18 +0200

    next commit

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -13,7 +13,7 @@ export class BlogListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const category01 = new Category('1', 'Car');
+    const category01 = new Category('1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
     const category02 = new Category('2', 'Flat');
     const category03 = new Category('3', 'Food');
 
